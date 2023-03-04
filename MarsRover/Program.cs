a0082e9b8a01b48afeb818be901542cb29d7e969
Roelf <roelf@apple.com>
2023-03-04 11:28:17 +0200

    Finished main

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.IO;
+using MarsRover.RoverNav;
 
 namespace MarsRover;
 public static class MarsRover
 {
   public static void Main(string[] args)
   {
-    Console.WriteLine("Toets 1 2 3");
+    var instructions = File.ReadAllText("./test-files/rover-instructions.txt");
+    var rovers = Mars.DriveRovers(instructions);
+    if (rovers.Count == 0) {
+      System.Console.WriteLine("Invalid instructions");
+      return;
+    }
+    System.Console.WriteLine("Final rover positions:");
+    rovers.ForEach(System.Console.WriteLine);
   }
 }
